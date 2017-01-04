5e852bd1c2224b9216e61f1a62fb87ede11c9b3f
Sang Choi <akasyashc@gmail.com>
2017-01-04 12:18:18 -0800

    API request to Imgur api endpoint

--- images/client/main.js
+++ images/client/main.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 import ImageList from './components/image_list'
 
-const App = () => {
-    return (
-        <div className="app">
-            <ImageList />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { api: [] };
+    }
+
+    componentWillMount() {
+        axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+            .then(response => this.setState({ api: response.data.data }));
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <ImageList />
+            </div>
+        );
+    }
 };
 
 Meteor.startup(() => {
