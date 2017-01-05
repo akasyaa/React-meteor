ec35d27f7d2ec7be75b4779c5107432888fd9b5c
Sang Choi <akasyashc@gmail.com>
2017-01-04 19:52:57 -0800

    Populate list with detail and add flexbox styling

--- profile/client/components/employee_list.js
+++ profile/client/components/employee_list.js
@@ -3,11 +3,15 @@ import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 
+import EmployeeDetail from './employee_detail';
+
 const EmployeeList = (props) => {
+    console.log(props.employees);
     return (
         <div>
             <div className="employee-list">
-                List
+                {props.employees.map(employee =>
+                    <EmployeeDetail key={employee._id} employee={employee} />)}
             </div>
         </div>
     )
