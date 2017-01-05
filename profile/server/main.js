6d8601bdce4cfcad2bc1a73876e66ef37af7e906
Sang Choi <akasyashc@gmail.com>
2017-01-04 18:55:50 -0800

    Initialize MongoDB collection and populate it with fake data

--- profile/server/main.js
+++ profile/server/main.js
@@ -1,5 +1,18 @@
+import _ from 'lodash';
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
+import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    const numberRecords = Employees.find({}).count();
+    if (!numberRecords) {
+        _.times(5000, () => {
+            const { name, email, phone } = helpers.createCard();
+
+            Employees.insert({
+                name, email, phone,
+                avatar: image.avatar()
+            });
+        });
+    }
 });
