9adf407cf58fb62ad1ba9a2afc745c678315fe2e
Sang Choi <akasyashc@gmail.com>
2017-01-05 18:38:26 -0800

    Bootstrap Header

--- url_shortner/client/main.js
+++ url_shortner/client/main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/header';
+
 class App extends Component {
     render() {
         return (
             <div>
-                App
+                <Header />
             </div>
         );
     }
